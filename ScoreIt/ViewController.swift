27310e303adab9889c3d78e384a63d1d1e4f6586
vik denic <vik.denic@gmail.com>
2016-10-17 13:42:21 -0500

    consolidate data calls to own model class

--- ScoreIt/ViewController.swift
+++ ScoreIt/ViewController.swift
@@ -13,19 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet var awayLabel: UILabel!
     @IBOutlet var homeLabel: UILabel!
 
-    let kMLBPrimaryKey = NSDictionary(contentsOfFile: Bundle.main.path(forResource: "Keys", ofType: "plist")!)?.object(forKey: "mlb_primary_key") as! String
-    let kGamesByDatePath = "https://api.fantasydata.net/mlb/v2/json/GamesByDate/2016-OCT-16"
-    let kSchedulesPath = "https://api.fantasydata.net/mlb/v2/json/Games/2016"
-    let kActiveTeamsPath = "https://api.fantasydata.net/mlb/v2/json/teams"
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        getData()
+        SportsData.games(forDate: "2016-OCT-18") { (games, error) in
+            guard let games = games else {
+                print(error)
+                return
+            }
+            print(games.count)
+        }
     }
 
-    func getData() {
+
+}
+
+let kMLBPrimaryKey = NSDictionary(contentsOfFile: Bundle.main.path(forResource: "Keys", ofType: "plist")!)?.object(forKey: "mlb_primary_key") as! String
+let kGamesByDatePath = "https://api.fantasydata.net/mlb/v2/json/GamesByDate/"
+let kSchedulesPath = "https://api.fantasydata.net/mlb/v2/json/Games/2016"
+let kActiveTeamsPath = "https://api.fantasydata.net/mlb/v2/json/teams"
+
+class SportsData {
+    /// Returns a list of Games for the specified date
+    ///
+    /// - parameter date:     The date to retrieve games for, in the format: 2015-JUL-31
+    /// - parameter complete: The completion block of the network call, returning games or an error
+    class func games(forDate date: String, complete : @escaping (_ games : [Game]?, _ error: Error?) -> Void) {
         //create path
-        var path = kGamesByDatePath as NSString
+        var path = kGamesByDatePath + date
         let params = ["entities=true"] as NSArray
 
         let string = params.componentsJoined(by: "&")
@@ -40,25 +54,24 @@ class ViewController: UIViewController {
         request.setValue(kMLBPrimaryKey, forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
 
         let task: URLSessionDataTask = session.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
-                do {
-                    if let data = data {
-                        let jsonArray = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [NSDictionary]
-                        for dict in jsonArray {
-                            print(dict)
-                            DispatchQueue.main.async {
-                                let game = Game(dict: dict)
-                                self.homeLabel.text = game.homeTeam! + " \(game.homeScore!)"
-                                self.awayLabel.text = game.awayTeam! + " \(game.awayScore!)"
-                            }
+            do {
+                if let data = data {
+                    let jsonArray = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [NSDictionary]
+                    var games = [Game]()
+                    for dict in jsonArray {
+                        let game = Game(dict: dict)
+                        games.append(game)
+                        DispatchQueue.main.async {
+                            complete(games, error)
                         }
                     }
-                } catch {
-
                 }
+            } catch {
+                complete(nil, error)
+            }
         }
         task.resume()
     }
-
 
 }
 
